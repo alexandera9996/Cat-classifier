2179b6ef96388dd7c3174ae1ef8f8255bf38e1d1
Alex <aangelid@ucsc.edu>
2018-08-06 21:38:19 -0700

    first push

---
+++ Cat_classifier.py
@@ -0,0 +1,156 @@
+import numpy as np
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
+
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+index = 25
+
+#print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
+
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+
+
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0],-1).T
+
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+
+
+def sigmoid(z):
+    s = 1/(1+np.exp(-z))
+    return s
+
+
+#print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
+
+def initialize_with_zeros(dim):
+
+    w = np.zeros((dim,1))
+    b = 0
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b
+
+dim = 2
+w, b = initialize_with_zeros(dim)
+
+def propagate(w, b, X, Y):
+
+    m = X.shape[1]
+
+    A = sigmoid(np.dot(w.T,X)+b)                                    # compute activation
+    cost = -1/m*(np.dot(Y,np.log(A).T)+np.dot((1-Y),np.log(1-A).T))                                   # compute cost
+
+    dw = 1/m*(np.dot(X,(A-Y).T))
+    db = 1/m*(np.sum(A-Y))
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+
+w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+grads, cost = propagate(w, b, X, Y)
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    costs = []
+    
+    for i in range(num_iterations):
+        
+        grads, cost = propagate(w, b, X, Y)
+
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        w = w-(np.dot(dw,learning_rate))
+        b = b-(learning_rate*db)
+
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        #if print_cost and i % 100 == 0:
+         #   print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+
+params, grads, costs = optimize(w, b, X, Y, num_iterations= 2000, learning_rate = 0.01, print_cost = False)
+
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+    
+    A = sigmoid(np.dot(w.T,X)+b)
+    
+    for i in range(A.shape[1]):
+        
+        if A[0,i] <= 0.5:
+            Y_prediction[0,i] = 0
+        else:
+            Y_prediction[0,i] = 1
+    
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
+
+w = np.array([[0.1124579],[0.23106775]])
+b = -0.3
+X = np.array([[1.,-1.1,-3.2],[1.2,2.,0.1]])
+#print ("predictions = " + str(predict(w, b, X)))
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 4000, learning_rate = 0.1, print_cost = False):
+    w, b = initialize_with_zeros(X_train.shape[0])
+
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations = 4000, learning_rate = 0.5, print_cost = False)
+    
+    w = parameters["w"]
+    b = parameters["b"]
+    
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+    
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+    return d
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 4000, learning_rate = 0.01, print_cost = True)
+
+costs = np.squeeze(d['costs'])
+
+my_image = "kobe2.jpg" 
+img = my_image  
+
+# We preprocess the image to fit your algorithm.
+fname = "images/" + my_image
+image = np.array(ndimage.imread(fname, flatten=False))
+my_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1, num_px*num_px*3)).T
+my_predicted_image = predict(d["w"], d["b"], my_image)
+
+print(img + " is a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture!")
